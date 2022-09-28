797ee6a4c2652c521ba64d08315cbe895cc7ae06
dibbodas12 <dibbodas228@gmail.com>
2022-09-28 21:22:49 +0600

    fake data created

---
+++ src/Shop/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div>
+            <div className='logo'>
+                <h1 className='header-text'>Fitness Freaks</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
